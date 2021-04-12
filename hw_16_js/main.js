e3baf619b504d96124e64aa609e367b9627fbced
therembo <etishkov12@gmail.com>
2021-04-12 15:52:44 +0300

    workwork

--- hw_16_js/main.js
+++ hw_16_js/main.js
@@ -1,11 +1,25 @@
-loginForm.loginButton.addEventListener("click", (event) => {
+// console.log(loginSignButton)
+loginSignButton.addEventListener("click", (event) => {
   event.preventDefault();
   console.log("privet");
   loginFormDiv.classList.add("hide");
   regFormDiv.classList.remove("hide");
-  regFormDiv.classList.add("active1");
+  regFormDiv.classList.add("active");
 });
-regForm.regButton.addEventListener("click", (event) => {
+ let baseData = []
+regForm.signRegButton.addEventListener("click", (event) => {
     event.preventDefault();
+      baseData = userReg(regName.value,regEmail.value,regLogin.value,regPassword.value)
+      console.log(baseData)
+      window.localStorage.setItem(`${baseData.login}`,`${JSON.stringify(baseData)}`)
+      
+    window.localStorage.setItem('name','Sanishua')
   });
-  
+function userReg(name,email,login,password){
+  return{
+    name:name,
+    email:email,
+    login:login,
+    password:password
+  } 
+}
